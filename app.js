58c21cfba03dc44cc2e3f99e58c037e6254efb07
harshitmourya <harshitmourya111@gmail.com>
2025-08-18 17:47:18 +0530

    deployed to vercel

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT||5000;
-app.listen(PORT ,()=>{
-console.log(`server is running on PORT ${PORT}`);
+// const PORT = process.env.PORT||5000;
+// app.listen(PORT ,()=>{
+// console.log(`server is running on PORT ${PORT}`);
 
-})
+// })
